ca8e6c02071050cdb35da369fe5c1ef9307c9586
soniagkalliath <soniageorge111@gmail.com>
2023-09-11 09:54:26 +0530

    EMS front end

---
+++ src/app/modules/users/users-list/users-list.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { UserSchema } from '../users.model';
+import jspdf from 'jspdf';
+import autoTable from 'jspdf-autotable';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent implements OnInit {
+
+  allUsers:UserSchema[]=[]
+  searchKey:string=""
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 5;
+
+  constructor(private api:ApiService){  }
+  //when UsersListComponent page is open call getUserList()
+  ngOnInit(): void {
+    this.getUserList()
+  }
+  getUserList(){
+     //When Users List page open, display all users list from json file by making an api call: http://localhost:3000/users
+    this.api.getallusers().subscribe({
+      next:(result:any)=>{
+        console.log(result); 
+        this.allUsers = result
+        // this.sortByName()
+        this.count = this.allUsers.length
+      },
+      error:(result:any)=>{
+        console.log(result);
+        alert("Error while fetching the data... Please try after some time!!!")
+      }
+    })
+  }
+
+  deleteuser(id:any){
+    this.api.deleteuser(id).subscribe({
+      next:(res:any)=>{
+        console.log("deleted successfully");
+        this.getUserList()        
+      },
+      error:(err:any)=>{
+        console.log(err);
+        alert("Cannot perform the action now... Please try after some times!!!")
+      }
+    })
+  }
+
+  sortById(){
+    this.allUsers.sort((a:any,b:any)=>a.id-b.id)
+  }
+
+ 
+  sortByName(){
+    this.allUsers.sort((a:any,b:any)=>a["name"].localeCompare(b["name"]) )
+  }
+
+  onTableDataChange(event: any) {
+    this.page = event;
+  }
+
+  generatePDF(){
+    let pdf = new jspdf()
+    let head = [['User Id','Username','Email','Status']]
+    let body:any = []
+    this.allUsers.forEach((item:any)=>{
+      body.push([item.id,item.name,item.email,item.active])
+    })
+    pdf.setFontSize(16)
+    pdf.text("All Employee List",10,10)
+    autoTable(pdf,{head,body})
+    pdf.output('dataurlnewwindow')
+    pdf.save("allUsers.pdf")
+  }
+
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserSchema } from '../modules/users/users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  base_url:string = "http://localhost:3000"
+
+  constructor(private http:HttpClient) { }
+
+  //get admin details
+  adminDetails(){
+    //api call to http://localhost:3000/users/1
+    return this.http.get(`${this.base_url}/users/1`)
+  }
+
+  //getallusers
+  getallusers(){
+      //api call to http://localhost:3000/users
+      return this.http.get(`${this.base_url}/users`)
+  }
+  
+
+  //adduser
+  addUser(user:UserSchema){
+    //api call to http://localhost:3000/users
+    return this.http.post(`${this.base_url}/users`,user)
+  }
+
+  //getexistinguser
+  getexistinguser(id:any){
+    //api call to http://localhost:3000/users/id
+    return this.http.get(`${this.base_url}/users/${id}`)
+  }
+
+  //updateuser
+  updateuser(id:any,data:UserSchema){
+    //api call to http://localhost:3000/users/id
+    return this.http.put(`${this.base_url}/users/${id}`,data)
+  }
+  
+  //deleteuser
+  deleteuser(id:any){
+    //api call to http://localhost:3000/users/id
+    return this.http.delete(`${this.base_url}/users/${id}`)
+  }
+
+  //updateadmin
+  updateAdmin(adminBody:UserSchema){
+    //api call to http://localhost:3000/users/1
+    return this.http.put(`${this.base_url}/users/1`,adminBody)
+  }
+
+}
